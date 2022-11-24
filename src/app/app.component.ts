cd9c0511a82cea8956e37e9ecc916773cc936f1e
Phạm Tiến Dũng <56197293+phamtiendungcw@users.noreply.github.com>
2022-11-25 05:18:25 +0700

    Revert "Revert "#1 - Building a walking skeleton""
    This reverts commit 7a609d0a34fe4d15f9bee82a76b31e620267b562.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  title = 'Message App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      (res) => {
+        this.users = res;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
