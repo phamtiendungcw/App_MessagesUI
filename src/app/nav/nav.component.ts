93270cbe4b7b4c28b8b98f70f9fc4242ff5af075
Phạm Tiến Dũng <56197293+phamtiendungcw@users.noreply.github.com>
2022-11-30 05:06:11 +0700

    Closes #9

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,33 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Observable } from 'rxjs';
-import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
-
-  ngOnInit(): void {
-  }
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   login() {
-    this.accountService.login(this.model).subscribe(
-      (res) => {
-        console.log(res);
+    this.accountService.login(this.model).subscribe({
+      next: (v) => {
+        this.router.navigateByUrl('/members').then((r) => r.valueOf());
       },
-      (error) => {
-        console.log(error);
-      }
-    );
+      error: (e) => {
+        console.log(e);
+        this.toastr.error(e.error);
+      },
+    });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/').then((r) => r.valueOf());
   }
 }
